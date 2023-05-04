af333ec9e6bcfd1ced4e96fb0c33568ba751ef26
Daniel Higgins <60156997+higg003@users.noreply.github.com>
2023-05-04 16:42:24 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,3 +109,31 @@ module "Kube01" {
 
   account_customizations_name = "sandbox"
 }
+  
+module "Crypto01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cloudops.support+180@sonymusic.com"
+    AccountName               = "sme-crypto-sandbox-01"
+    ManagedOrganizationalUnit = "SME-SANDBOX"
+    SSOUserEmail              = "cloudops.support+180@sonymusic.com"
+    SSOUserFirstName          = "RDC"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
