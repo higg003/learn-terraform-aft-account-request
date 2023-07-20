79eb5eeb750cd762626d0b6223b6c409df18086f
Daniel Higgins <60156997+higg003@users.noreply.github.com>
2023-07-20 08:57:59 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,34 @@ module "DashStats01" {
 
   account_customizations_name = "sandbox"
 }
+
+module "DashStats02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cloudops.support+186@sonymusic.com"
+    AccountName               = "sme-dash-sandbox-02"
+    ManagedOrganizationalUnit = "SME-SANDBOX"
+    SSOUserEmail              = "cloudops.support+186@sonymusic.com"
+    SSOUserFirstName          = "DashStats"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
   
 module "Carma01" {
   source = "./modules/aft-account-request"
