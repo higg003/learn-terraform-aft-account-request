a489aa67ad9063f824ae398fdfe584ef75861620
Daniel Higgins <60156997+higg003@users.noreply.github.com>
2023-08-07 11:25:05 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -262,7 +262,33 @@ module "Scuba-aft" {
 
   account_customizations_name = "sandbox"
 }
+module "Scuba-aft" {
+  source = "./modules/aft-account-request"
 
+  control_tower_parameters = {
+    AccountEmail              = "cloudops.support+198@sonymusic.com"
+    AccountName               = "sme-sam-sandbox-03"
+    ManagedOrganizationalUnit = "SME-SANDBOX"
+    SSOUserEmail              = "cloudops.support+198@sonymusic.com"
+    SSOUserFirstName          = "Scuba-GT"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
 module "Max-aft" {
   source = "./modules/aft-account-request"
 
